c840a73a768a3a318b04c24e85b0cfa5f0ab18f3
Gurjot Singh <147967653+gurjot03@users.noreply.github.com>
2025-05-05 00:56:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def main():
 
     st.title("Testing Solutions Search App")
     
-    mongodb_uri = st.secrets['MONGODB_URI]
+    mongodb_uri = st.secrets['MONGODB_URI']
     
     search_system = AssessmentSearchSystem(mongodb_uri)
 
